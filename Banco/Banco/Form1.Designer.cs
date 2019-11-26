2691e062edf303ff9f77f867d12b4f38dfb2a9e7
elotronacho <53498193+elotronacho@users.noreply.github.com>
2019-11-26 13:15:17 -0300

    Generadores de Números Aleatorio

--- Banco/Banco/Form1.Designer.cs
+++ Banco/Banco/Form1.Designer.cs
@@ -28,12 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtBoxFrecuencia = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtBoxA = new System.Windows.Forms.TextBox();
+            this.txtBoxB = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtBoxMedia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(106, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Llegada de Clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(446, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Atención de Cajeros";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(600, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cajero 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(368, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cajero 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label5.Location = new System.Drawing.Point(323, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 39);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Banco";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "(Exponencial negativa)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(345, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "(Exponencial negativa)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(613, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "(Uniforme)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(55, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Frecuencia: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(233, 131);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Clientes/h";
+            // 
+            // txtBoxFrecuencia
+            // 
+            this.txtBoxFrecuencia.Location = new System.Drawing.Point(127, 128);
+            this.txtBoxFrecuencia.Name = "txtBoxFrecuencia";
+            this.txtBoxFrecuencia.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxFrecuencia.TabIndex = 10;
+            this.txtBoxFrecuencia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxFrecuencia_KeyPress);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(613, 128);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "A:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(613, 153);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(17, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "B:";
+            // 
+            // txtBoxA
+            // 
+            this.txtBoxA.Location = new System.Drawing.Point(633, 125);
+            this.txtBoxA.Name = "txtBoxA";
+            this.txtBoxA.Size = new System.Drawing.Size(71, 20);
+            this.txtBoxA.TabIndex = 13;
+            this.txtBoxA.TextChanged += new System.EventHandler(this.txtBoxA_TextChanged);
+            this.txtBoxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxA_KeyPress);
+            // 
+            // txtBoxB
+            // 
+            this.txtBoxB.Location = new System.Drawing.Point(633, 150);
+            this.txtBoxB.Name = "txtBoxB";
+            this.txtBoxB.Size = new System.Drawing.Size(71, 20);
+            this.txtBoxB.TabIndex = 14;
+            this.txtBoxB.TextChanged += new System.EventHandler(this.txtBoxB_TextChanged);
+            this.txtBoxB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxB_KeyPress);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(345, 131);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(42, 13);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Media: ";
+            // 
+            // txtBoxMedia
+            // 
+            this.txtBoxMedia.Location = new System.Drawing.Point(393, 128);
+            this.txtBoxMedia.Name = "txtBoxMedia";
+            this.txtBoxMedia.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxMedia.TabIndex = 16;
+            this.txtBoxMedia.TextChanged += new System.EventHandler(this.txtBoxMedia_TextChanged);
+            this.txtBoxMedia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxMedia_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 338);
+            this.Controls.Add(this.txtBoxMedia);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtBoxB);
+            this.Controls.Add(this.txtBoxA);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtBoxFrecuencia);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtBoxFrecuencia;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtBoxA;
+        private System.Windows.Forms.TextBox txtBoxB;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtBoxMedia;
     }
 }
 
--- Banco/Banco/Form1.cs
+++ Banco/Banco/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace Banco
 {
@@ -16,5 +18,203 @@ namespace Banco
         {
             InitializeComponent();
         }
+
+        //private void btnLlegadaCliente_Click(object sender, EventArgs e)
+        //{
+        //    lblLlegadaCliente.Text = tiempoEntreLlegadas(int.Parse(txtBoxFrecuencia.Text)).ToString();
+        //}
+
+        private void txtBoxFrecuencia_KeyPress(object sender, KeyPressEventArgs e) 
+        {
+            //Para obligar a que sólo se introduzcan números
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+              if (Char.IsControl(e.KeyChar)) //permitir teclas de control como retroceso
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                //el resto de teclas pulsadas se desactivan
+                e.Handled = true;
+            }
+
+        }
+       
+
+        private void txtBoxMedia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 8)
+            {
+                e.Handled = false;
+                return;
+            }
+
+
+            bool IsDec = false;
+            int nroDec = 0;
+
+            for (int i = 0; i < txtBoxMedia.Text.Length; i++)
+            {
+                if (txtBoxMedia.Text[i] == '.')
+                    IsDec = true;
+
+                if (IsDec && nroDec++ >= 2)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+
+            }
+
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+                e.Handled = false;
+            else if (e.KeyChar == 46)
+                e.Handled = (IsDec) ? true : false;
+            else
+                e.Handled = true;
+
+        }
+
+        private void txtBoxMedia_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+       
+
+        private void txtBoxA_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtBoxA_KeyPress(object sender, KeyPressEventArgs e)
+       {
+            if (e.KeyChar == 8)
+            {
+                e.Handled = false;
+                return;
+            }
+
+
+            bool IsDec = false;
+            int nroDec = 0;
+
+            for (int i = 0; i < txtBoxA.Text.Length; i++)
+            {
+                if (txtBoxA.Text[i] == '.')
+                    IsDec = true;
+
+                if (IsDec && nroDec++ >= 2)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+
+            }
+
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+                e.Handled = false;
+            else if (e.KeyChar == 46)
+                e.Handled = (IsDec) ? true : false;
+            else
+                e.Handled = true;
+
+        }
+
+
+        private void txtBoxB_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtBoxB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 8)
+            {
+                e.Handled = false;
+                return;
+            }
+
+
+            bool IsDec = false;
+            int nroDec = 0;
+
+            for (int i = 0; i < txtBoxB.Text.Length; i++)
+            {
+                if (txtBoxB.Text[i] == '.')
+                    IsDec = true;
+
+                if (IsDec && nroDec++ >= 2)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+
+            }
+
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+                e.Handled = false;
+            else if (e.KeyChar == 46)
+                e.Handled = (IsDec) ? true : false;
+            else
+                e.Handled = true;
+
+        }
+
+        decimal tiempoEntreLlegadas(int frecuencia)
+        {
+            //Creo random 0-1
+            Random _Random = new Random();
+            double rnd = _Random.NextDouble();
+
+            double frecuenciaClientesPorMinuto = frecuencia*0.016666666666;
+            double x;
+            double cociente;
+            cociente = -1 / frecuenciaClientesPorMinuto;
+            x = cociente * Math.Log(1 - rnd);
+            decimal redondeado = (decimal)Math.Round(x, 3);
+            return redondeado;
+        }
+
+        decimal tiempoCajero1(double media)
+        {
+            Random _Random = new Random();
+            double rnd = _Random.NextDouble();
+
+            double x = (double)(-media) * Math.Log(1 - rnd);
+            decimal redondeado = (decimal)Math.Round(x, 3);
+            return redondeado;
+        }
+        decimal tiempoCajero2(double a, double b)
+        {
+            Random _Random = new Random();
+            double rnd = _Random.NextDouble();
+
+            double x = (rnd * (b - a)) + a;
+            decimal redondeado = (decimal)Math.Round(x, 3);
+            return redondeado;
+        }
+
+        //private void btnCajero1_Click(object sender, EventArgs e)
+        //{
+            
+        //    double x = float.Parse(txtBoxMedia.Text, CultureInfo.CreateSpecificCulture("en-US"));
+        //    lblCajero1.Text = tiempoCajero1(x).ToString();
+        //    //lblCajero1.Text = tiempoCajero1(Convert.ToDouble(txtBoxMedia.Text)).ToString();
+        //}
+
+        //private void btnCajero2_Click(object sender, EventArgs e)
+        //{
+        //    double a = float.Parse(txtBoxA.Text, CultureInfo.CreateSpecificCulture("en-US"));
+        //    double b = float.Parse(txtBoxB.Text, CultureInfo.CreateSpecificCulture("en-US"));
+        //    lblCajero2.Text = tiempoCajero2(a, b).ToString();
+
+        //}
     }
 }
